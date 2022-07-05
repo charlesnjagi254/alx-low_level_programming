dbd85660d870bff06ff5707c880d9bac1fd9cd47
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-07-04 17:06:05 -0700

    Task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints out all the members under 10 with a,
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for ( i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
