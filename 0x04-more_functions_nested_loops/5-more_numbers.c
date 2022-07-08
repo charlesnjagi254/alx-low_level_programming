da90baffefe2d3bb018ed0bc32179870365814ad
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-07-08 14:02:03 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers(void)
+ *
+ * Return: no return
+ */
+void more_numbers(void)
+{
+	char i;
+	int k;
+	int f;
+	int l;
+	for (i = '0'; i <= '0'; i++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			f = k / 10;
+			l = k % 10;
+			if (k >=10)
+				_putchar (f+ '0');
+			if (k -- 10)
+				1 = 0;
+			_putchar(1 + '0');
+		}
+		_putchar('\n';
+	}
+}
+
+
+
