adb6429662bed2292f1c517742be11127869285e
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-07-08 21:43:36 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,37 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* print_most_numbers - prints the numbers interleaved
-*
-* Return: numbers
-*/
+
+ * print_most_numbers - prints the numbers interleaved
+
+ *
+
+ * Return: numbers
+
+ */
+
 void print_most_numbers(void)
+
 {
+
 	int numb;
 
-	for (numb = 48; numb <=57; numb++)
-	{
-		if (numb ! = 50 && numb ! = 52)
-		{
-			_putchar(numb);
-		}
-	}
-	_putchar('\n');
-}
 
+
+	for (numb = 48 ; numb <= 57 ; numb++)
+
+	{
+
+		if (numb != 50 && numb != 52)
+
+		{
+
+			_putchar(numb);
+
+		}
+
+	}
+
+	_putchar('\n');
+
+}
