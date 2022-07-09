be62edcda8107c2be1d142df6122d09bc5f6704b
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-07-08 21:36:32 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,31 +1,57 @@
 #include "main.h"
 
+
+
 /**
- * more_numbers(void)
+
+ * more_numbers - Entry point
+
  *
+
  * Return: no return
+
  */
+
 void more_numbers(void)
+
 {
+
 	char i;
+
 	int k;
+
 	int f;
+
 	int l;
-	for (i = '0'; i <= '0'; i++)
+
+
+
+	for (i = '0'; i <= '9'; i++)
+
 	{
+
 		for (k = 0; k <= 14; k++)
+
 		{
+
 			f = k / 10;
+
 			l = k % 10;
-			if (k >=10)
-				_putchar (f+ '0');
-			if (k -- 10)
-				1 = 0;
-			_putchar(1 + '0');
+
+			if (k >= 10)
+
+			_putchar (f + '0');
+
+			if (k == 10)
+
+				l = 0;
+
+			_putchar(l + '0');
+
 		}
-		_putchar('\n';
+
+		_putchar('\n');
+
 	}
+
 }
-
-
-
