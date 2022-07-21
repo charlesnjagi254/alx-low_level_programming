df64874a3ba33ad71684e3710ad6148c1f8fc5ff
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-07-21 13:47:38 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,20 +1,39 @@
 /**
- *  _pow_recursion - Performs power of a mater
- *  @x: Integer Value
- *  @y: Power Value
- *  Return: Factorial
+
+ * _pow_recursion - Performs power of a number
+
+ * @x: Integer Value
+
+ * @y: Power Value
+
+ * Return: Factorial
+
  */
+
 int _pow_recursion(int x, int y)
+
 {
-	if (y<0);
+
+	if (y < 0)
+
 	{
-		return (-1):
+
+		return (-1);
+
 	}
+
 	else
+
 	{
-		if (y>0)
+
+		if (y > 0)
+
 			return (x * (_pow_recursion(x, y - 1)));
+
 		else
+
 			return (1);
+
 	}
+
 }
