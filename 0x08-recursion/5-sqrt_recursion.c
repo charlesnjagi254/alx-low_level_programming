30d24d32421c393d07302067604f4d67c4948229
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-07-21 09:40:18 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * sqtchecker - computes square root recursively doing binary search
+ * @min: initial number
+ * @max: last number within the limit of number
+ * @m: given number
+ **/
+int sqtChecker(int min, int max, int m)
+{
+	long int guess;
+	if (max >= min)
+	{
+		guess = min + (max - min) / 2;
+		if (guess * guess == m)
+			return (guess);
+/* following binary search */
+		if (guess * guess > m)
+			return (sqtChecker(min, guess - 1, m));
+		if (guess * guess < m)
+			return (sqtChecker(guess + 1, max, m));
+	}
+	return (-1);
+}
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: given number
+ * Return: square root of n or -1
+ **/
+	int _sqrt_recursion(in n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return (sqtChecker(1, n, n));
+}
