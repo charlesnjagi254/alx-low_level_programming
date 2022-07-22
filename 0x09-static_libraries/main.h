c54de53ba5b3dccbe029bd21be72338560392929
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-07-22 14:29:18 -0700

    library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -37,3 +40,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
+#endif
